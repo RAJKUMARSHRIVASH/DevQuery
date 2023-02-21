35345b9bd20078ded8d9ca31da75492b9ad197aa
RAJ KUMAR SEN <rajkumarshrivash374@gmail.com>
2023-02-22 00:39:35 +0530

    Roles added in the UserModel

--- backend/model/UserModel.js
+++ backend/model/UserModel.js
@@ -1,1 +1,17 @@
 const mongoose = require('mongoose');
+
+// <<-------------------------Roll based access control System--------------->>
+const userSchema = mongoose.Schema({
+    name:String,
+    email:String,
+    password:String,
+    role :{
+        type : String, enum : ["User","Developer","Admin"], default : "User"
+    }
+});
+
+const UserModel = mongoose.model("user",userSchema);
+
+module.exports = {
+    UserModel
+}
