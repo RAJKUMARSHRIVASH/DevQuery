9f08dae550a3098274ca944b9ab301cacb734e64
RAJ KUMAR SEN <rajkumarshrivash374@gmail.com>
2023-02-22 00:02:41 +0530

    Files added

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 require('dotenv').config();
 const connection = require("./config/db");
+const userRoute = require("./routes/userRoute");
 
 app.use(express.json());
 
