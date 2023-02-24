4561e2ede43d50e577582fa3b2e21b4d0e92d340
DhaanuI <dhaanu97@gmail.com>
2023-02-24 17:18:53 +0530

    ansroute

--- backend/routes/quesRoute.js
+++ backend/routes/quesRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const quesRoute = express.Router();
 const { QuesModel } = require("../model/questionModel");
-const {authenticate} = require("../middleware/authenticate")
+const { authenticate } = require("../middleware/authenticate")
 
 
 quesRoute.get("/", async (req, res) => {
@@ -10,41 +10,59 @@ quesRoute.get("/", async (req, res) => {
 })
 
 quesRoute.get("/sortbydescending", async (req, res) => {
-    let data = await QuesModel.find().sort({posted:-1})
+    let data = await QuesModel.find().sort({ posted: -1 })
     res.send(data)
 })
 
 quesRoute.get("/sortbyascending", async (req, res) => {
-    let data = await QuesModel.find().sort({posted:1})
+    let data = await QuesModel.find().sort({ posted: 1 })
     res.send(data)
 })
 
-quesRoute.post("/addquestion",authenticate, async (req, res) => {
-    const {name, question, userID} = req.body;
+quesRoute.get("/sortbyanswercount", async (req, res) => {
+    let data = await QuesModel.aggregate([
+        {
+            "$project": {
+                "name": 1,
+                "question": 1,
+                "userID": 1,
+                "answer": 1,
+                "posted": 1,
+                "length": { "$size": "$answer" }
+            }
+        },
+        { "$sort": { "length": -1 } }
+    ]);
+    console.log(data)
+    res.send(data)
+})
+
+quesRoute.post("/addquestion", authenticate, async (req, res) => {
+    const { name, question, userID } = req.body;
     try {
         const new_question = new QuesModel({
             name,
             question,
             userID
-          });
-         await new_question.save();
-         res.json(new_question)
+        });
+        await new_question.save();
+        res.json(new_question)
     } catch (error) {
-       console.log(error) 
+        console.log(error)
     }
 })
 
-quesRoute.post("/addans/:id",authenticate, async (req, res) => {
+quesRoute.post("/addans/:id", authenticate, async (req, res) => {
     const quesID = req.params.id;
-    const {name, answer, userID, questionaskerID} = req.body;
-    if(userID == questionaskerID){
-        res.json({"mas": "You cann't answer your own questions"})
+    const { name, answer, userID, questionaskerID } = req.body;
+    if (userID == questionaskerID) {
+        res.json({ "mas": "You cann't answer your own questions" })
     }
     try {
-        const add_ans = await QuesModel.updateOne({ "_id": quesID}, {$push: {"answer": {name, answer, userID}}})
+        const add_ans = await QuesModel.updateOne({ "_id": quesID }, { $push: { "answer": { name, answer, userID } } })
         res.json(add_ans)
     } catch (error) {
-       console.log(error) 
+        console.log(error)
     }
 })
 
--- frontend/script/home.js
+++ frontend/script/home.js
@@ -1,9 +1,13 @@
 
 
+document.getElementById("askques").addEventListener("click", () => {
+    window.location.href = "ask.html"
+})
+
 let Maindiv = document.getElementById("renderhere");
 
+// all about fetching and rendering data
 getData()
-
 async function getData() {
     let data = await fetch("http://localhost:8000/questions/");
     let res = await data.json();
@@ -17,7 +21,9 @@ function renderData(array) {
         let div = document.createElement("div")
         div.setAttribute("class", "box")
         let ansdiv = document.createElement("div")
+        ansdiv.setAttribute("class", "abox")
         let quesdiv = document.createElement("div")
+        quesdiv.setAttribute("class", "qbox")
 
         let heading = document.createElement("p")
         heading.innerHTML = `<p style="display:inline;color:blue">${item.question.heading}</p>`;
@@ -31,7 +37,8 @@ function renderData(array) {
         let user = document.createElement("p")
         let time = document.createElement("p")
         let datetime = new Date(item.posted);
-        user.innerHTML = `<p style="display:inline;">Asked by</p> <p style="display:inline;color:blue">${item.name}</p> <p style="display:inline;">on</p> <p style="display:inline;color:blue">${datetime.getDate() + '-' + (datetime.getMonth() + 1) + '-' + datetime.getFullYear()}</p>`;
+        // user.innerHTML = `<p style="display:inline;">Asked by</p> <p style="display:inline;color:blue">${item.name}</p> <p style="display:inline;">on</p> <p style="display:inline;color:blue">${datetime.getDate() + '-' + (datetime.getMonth() + 1) + '-' + datetime.getFullYear()}</p>`;
+        user.innerHTML = `<p style="display:inline;">Asked by</p> <p style="display:inline;color:blue">${item.name}</p> <p style="display:inline;">on</p> <p style="display:inline;color:blue">${datetime.toLocaleString()}</p>`;
 
         let line = document.createElement("hr")
 
@@ -48,6 +55,8 @@ function renderData(array) {
     })
 }
 
+
+// redirection to specific question
 function clickedQuestion(event) {
     // console.log(event)
     console.log(event.target.parentElement.id)
@@ -68,6 +77,7 @@ function clickedQuestion(event) {
 // Sorting
 let sortDesc = document.getElementById("latest");
 let sortAsc = document.getElementById("old");
+let mostAnswered=document.getElementById("hot");
 
 sortDesc.addEventListener("click", async () => {
     let data = await fetch("http://localhost:8000/questions/sortbydescending");
@@ -87,3 +97,10 @@ sortAsc.addEventListener("click", async () => {
 })
 
 
+mostAnswered.addEventListener("click", async () => {
+    let data = await fetch("http://localhost:8000/questions/sortbyanswercount");
+    let res = await data.json();
+    console.log(res)
+    Maindiv.innerHTML = ""
+    renderData(res);
+})
